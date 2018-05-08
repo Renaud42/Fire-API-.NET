f98f216371145902f75339e27c2350a3801c691c
Renaud42 <renaud.4243@gmail.com>
2018-05-08 11:30:53 +0200

    Fire-Auth discontinued, approaching first version.

--- AuthObject.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class AuthObject
-
-End Class
--- Fire-API.ref.vb
+++ Fire-API.ref.vb
@@ -16,6 +16,110 @@ Public Class Fire_API_ref
         End If
     End Sub
 
+    ''' <summary>
+    ''' Type of server info required.
+    ''' </summary>
+    Public Enum ServerInfoType
+        CPU_USE_1
+        CPU_USE_2
+        CPU_USE_AVERAGE
+        DISK_MAX
+        DISK_PERCENT
+        DISK_UNIT
+        DISK_USED
+        HOSTNAME
+        MUMBLE_ONLINE
+        MUMBLE_USERS
+        RAM_MAX
+        RAM_PERCENT
+        RAM_UNIT
+        RAM_USED
+        SERVER_ONLINE
+    End Enum
+
+    ''' <summary>
+    ''' Return a specified information of the server.
+    ''' </summary>
+    ''' <param name="InfoType">Type of information needed.</param>
+    Public Shared Function GetServerInformation(InfoType As ServerInfoType)
+        ' Initializing the JSON file corresponding to the server of the specified ServerInfoType.
+        Initialize(InfoType)
+
+        Dim jss As New Web.Script.Serialization.JavaScriptSerializer
+        Dim response As Object
+
+        response = jss.DeserializeObject(IO.File.ReadAllText(Constants.API_Folder & Constants.API_ApplicationName & "\temp.json"))
+
+        Select Case InfoType
+            Case ServerInfoType.CPU_USE_1
+                Return response("status")("cpu")("1")
+            Case ServerInfoType.CPU_USE_2
+                Return response("status")("cpu")("2")
+            Case ServerInfoType.CPU_USE_AVERAGE
+                Return response("status")("cpu")("0")
+            Case ServerInfoType.DISK_MAX
+                Return response("status")("disk")("total")
+            Case ServerInfoType.DISK_PERCENT
+                Return response("status")("disk")("percent")
+            Case ServerInfoType.DISK_UNIT
+                Return response("status")("units")("disk")
+            Case ServerInfoType.DISK_USED
+                Return response("status")("disk")("used")
+            Case ServerInfoType.HOSTNAME
+                Return response("status")("hostname")
+            Case ServerInfoType.MUMBLE_ONLINE
+                Return response("status")("online")
+            Case ServerInfoType.MUMBLE_USERS
+                Return response("status")("players")("online")
+            Case ServerInfoType.RAM_MAX
+                Return response("status")("ram")("total")
+            Case ServerInfoType.RAM_PERCENT
+                Return response("status")("ram")("percent")
+            Case ServerInfoType.RAM_UNIT
+                Return response("status")("units")("ram")
+            Case ServerInfoType.RAM_USED
+                Return response("status")("ram")("used")
+            Case ServerInfoType.SERVER_ONLINE
+                Return response("status")("online")
+            Case Else
+                Throw New Exception("Invalid ServerInfoType.")
+        End Select
+    End Function
+
+    ''' <summary>
+    ''' Initialize one JSON status file.
+    ''' </summary>
+    ''' <param name="InfoType"><seealso cref="ServerInfoType"/> to get file for.</param>
+    ''' <param name="NetworkCheck">Check if an network connection is available else returns an <seealso cref="Exception"/> with message "Can't connect to the network."</param>
+    Public Shared Sub Initialize(InfoType As ServerInfoType, Optional NetworkCheck As Boolean = True)
+        If NetworkCheck Then
+            If My.Computer.Network.IsAvailable Then
+                InitializationWorker(InfoType)
+            Else
+                Throw New Exception("Can't connect to the network.")
+            End If
+        Else
+            InitializationWorker(InfoType)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Initialization worker.
+    ''' </summary>
+    ''' <param name="InfoType"><seealso cref="ServerInfoType"/> to get file for.</param>
+    Private Shared Sub InitializationWorker(InfoType As ServerInfoType)
+        Select Case InfoType
+            Case ServerInfoType.CPU_USE_1, ServerInfoType.CPU_USE_2, ServerInfoType.CPU_USE_AVERAGE, ServerInfoType.DISK_MAX, ServerInfoType.DISK_PERCENT, ServerInfoType.DISK_UNIT, ServerInfoType.DISK_USED, ServerInfoType.HOSTNAME, ServerInfoType.RAM_MAX, ServerInfoType.RAM_PERCENT, ServerInfoType.RAM_UNIT, ServerInfoType.RAM_USED, ServerInfoType.SERVER_ONLINE
+                Fire_API_ref.DestroyTempJSON()
+                My.Computer.Network.DownloadFile(Constants.JSON_VPS, Constants.API_Folder & Constants.API_ApplicationName & "\temp.json")
+            Case ServerInfoType.MUMBLE_ONLINE, ServerInfoType.MUMBLE_USERS
+                Fire_API_ref.DestroyTempJSON()
+                My.Computer.Network.DownloadFile(Constants.JSON_MUMBLE, Constants.API_Folder & Constants.API_ApplicationName & "\temp.json")
+            Case Else
+                Throw New Exception("Can't initialize the file corresponding to ServerInfoType """ & InfoType & """")
+        End Select
+    End Sub
+
 
     ''''''''''''''''''''''''''''''''''''''''''''''''''''
     ''                  NEW INSTANCE                  ''
@@ -29,8 +133,11 @@ Public Class Fire_API_ref
         Constants.API_ApplicationName = ApplicationName
 
         ' Create Fire-API directory if he's not existing.
+        If IO.Directory.Exists(Constants.API_Folder.Substring(0, Constants.API_Folder.Length - 9)) Then
+            IO.Directory.CreateDirectory(Constants.API_Folder.Substring(0, Constants.API_Folder.Length - 9))
+        End If
         If IO.Directory.Exists(Constants.API_Folder) = False Then
-            IO.Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Fire-API\")
+            IO.Directory.CreateDirectory(Constants.API_Folder)
         End If
         If IO.Directory.Exists(Constants.API_Folder & ApplicationName & "\") = False Then
             IO.Directory.CreateDirectory(Constants.API_Folder & ApplicationName & "\")
